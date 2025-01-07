949aae7b3b4bb46e938170aa420942a006340838
rishithabhupathiraju12 <rishithabhupathiraju1@gmail.com>
2025-01-07 19:31:44 +0530

    message

---
+++ Day5_loops.java
@@ -0,0 +1,38 @@
+public class Day5_loops{
+    public static void main(String[] args){
+        for(int i=11; i<=19; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
+
+
+//public String stringTimes(String str, int n) {
+    String result="";
+    for(int i=0;i<n;i++){
+      result = result + str;
+      
+    }
+    return result;
+  }
+
+
+  //public String frontTimes(String str, int n) {
+    String result="";
+    int end=3;
+    if(str.length()<3){
+      end=str.length();
+    }
+    
+    
+    
+    String front=str.substring(0,end);
+    for(int i=0;i<n;i++){
+      result = result+front;
+    }
+    return result;
+  }
+  
